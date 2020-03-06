daf6b20c21869c96f9a14fc6828214a64b05de59
Kejvi <kdoko@ritech.co>
2020-03-06 11:58:23 +0100

    search for hotel

--- UITesting/UITesting/Booking/Page/SearchHotelPage.cs
+++ UITesting/UITesting/Booking/Page/SearchHotelPage.cs
@@ -22,10 +22,13 @@ namespace UITesting.Booking.Page
         private IWebElement checkoutDateTextBox => this.driver.FindElement(By.Id("checkout"));
         private IWebElement adultsNoTextBox => this.driver.FindElement(By.Name("adults"));
         private IWebElement childrenNoTextBox => this.driver.FindElement(By.Name("children"));
+        private IWebElement searchButton => this.driver.FindElement(By.XPath("//div[@id='hotels']/div/div/form/div/div/div[4]/button"));
         private ReadOnlyCollection<IWebElement> destinationListDrowpDown => this.driver.FindElements(By.ClassName("select2-result-sub"));
+        
         public void SearchHotel(string destination, DateTime checkinDate, DateTime checkoutDate, int adultsNo, int childNo)
         {
             this.driver.Navigate().GoToUrl("https://www.phptravels.net/home");
+            this.driver.Manage().Window.Maximize();
 
             destinationDropDown.Click();
             destinationTextBox.SendKeys(destination);
@@ -36,6 +39,18 @@ namespace UITesting.Booking.Page
                 SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("select2-result-sub")));
 
             destinationListDrowpDown.First().Click();
+            checkinDateTextBox.Clear();
+            checkoutDateTextBox.Clear();
+            checkinDateTextBox.SendKeys(checkinDate.ToString("dd/MM/yyyy"));
+            checkoutDateTextBox.SendKeys(checkoutDate.ToString("dd/MM/yyyy"));
+            adultsNoTextBox.SendKeys($"{adultsNo}");
+            childrenNoTextBox.SendKeys($"{childNo}");
+            searchButton.Click();
+
+            // Wait for the search to finish
+            wait.Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName(@"sidebar-title")));
+           
 
         }
     }
